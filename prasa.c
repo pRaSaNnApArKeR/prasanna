a5e3a703d6b2d7dee57ca6c2897781fd054907e5
pRaSaNnApArKeR <35995008+pRaSaNnApArKeR@users.noreply.github.com>
2018-01-31 16:25:38 +0530

    prasann

---
+++ prasa.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+clrscsr();
+printf("enter the number");
+scanf("%d",&a);
+b=a/2;
+if (b=0)
+printf({"the number is even");
+else
+perintf("the number is odd");
+getch();
+return 0;
+}
