3fa3a8b98060e7c58bd80b3dc8524c9849b79d74
pRaSaNnApArKeR <35995008+pRaSaNnApArKeR@users.noreply.github.com>
2018-01-31 16:15:04 +0530

    prasanna.c

---
+++ prasanna.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+clrscr();
+if(a>0)
+printf("positive");
+else if(a<0)
+printf("negative");
+else 
+printf("zero");
+getch();
+return 0;
+}
